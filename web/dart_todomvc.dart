1bf5f37d2c6d05ee12426049f21a32c111cf3cbf
Kevin Weber <kevin@synapsegarden.net>
2014-10-19 23:52:12 -0400

    Attempted adding multiple todo lists. Seems like I'll need to start using Polymer to continue

--- web/dart_todomvc.dart
+++ web/dart_todomvc.dart
@@ -1,18 +1,31 @@
 import 'dart:html';
 
-InputElement todoInput;
+InputElement todoItemAdd;
+InputElement todoListAdd;
 UListElement todoItems;
+UListElement todoLists;
 
 void main() {
-  todoInput = querySelector('#todo-input');
+  todoItemAdd = querySelector('#todo-new-item');
   todoItems = querySelector('#todo-items');
   
-  todoInput.onChange.listen( addItem );
+  todoListAdd = querySelector('#todo-new-list');
+  todoLists = querySelector('#todo-lists');
+  
+  todoItemAdd.onChange.listen( addItem );
+  todoListAdd.onChange.listen( addList );
 }
 
 void addItem( Event e ){
     var newItem = new UListElement();
-    newItem.text = "" + todoInput.value;
-    todoInput.value = '';
+    newItem.text = "" + todoItemAdd.value;
+    todoItemAdd.value = '';
     todoItems.children.add(newItem);
+}
+
+void addList( Event e ){
+    var newList = new UListElement();
+    newList.text = "" + todoListAdd.value;
+    todoListAdd.value = '';
+    todoLists.children.add(newList);
 }
